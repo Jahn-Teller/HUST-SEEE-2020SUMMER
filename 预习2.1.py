f2f4517eed1b8461640da5a9dac4a464a634bf9b
Jahn-Teller <69451938+Jahn-Teller@users.noreply.github.com>
2020-08-12 10:53:53 +0800

    Update 预习2.1.py

--- 预习2.1.py
+++ 预习2.1.py
@@ -1,8 +1,8 @@
-_usrname=input("请初始化用户名:")
-_password=input("请初始化登录密码:")
+Usrname=input("请初始化用户名:")
+Password=input("请初始化登录密码:")
 #打印初始化后的用户名和密码
-print (_usrname)
-print (_password)
+print (Usrname)
+print (Password)
 #flag0代表输入错误的次数
 #flag1为用户名和密码都输入正确的标志
 flag0=0
@@ -11,11 +11,11 @@ print("----------用户登录----------")
 #主循环，当flag1=1时打印登陆成功并跳出
 while True:
     user=input("请输入用户名:")
-    if user==_usrname:
+    if user==Usrname:
         print("用户名输入正确")
         while flag0<3:
             password=input("请输入密码:")
-            if password==_password:
+            if password==Password:
                 print("----------登录成功----------")
                 flag1=1
                 break
